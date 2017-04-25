57b7cbdd744f0f200de32636e11827ae1e0b0ffb
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-25 20:40:07 +0200

    add eventListener on window resize to put right placeholder to dataMounth

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function checkScreen() {
-    var placeholder = window.innerWidth > 900 ? 'January' : '01';
-    const dateMounth = document.getElementById('dateMounth');
-    dateMounth.setAttribute('placeholder', placeholder);
+const model = {
+    "dateMounth" : document.getElementById('dateMounth'),
 }
 
-checkScreen();
+const operator = {
+    "checkWindowWidth" : function() {
+        var placeholder = window.innerWidth > 900 ? 'January' : '01';
+        model.dateMounth.setAttribute('placeholder', placeholder);
+    },
+    "eventListeners" : function() {
+        var windowWidth  = window.innerWidth;
+        window.addEventListener('resize', function() {
+            operator.checkWindowWidth();
+        });
+    }
+}
+
+const view = {
+    "inittCheck" : function() {
+        operator.checkWindowWidth();
+        operator.eventListeners();
+    }
+}
+view.inittCheck();
