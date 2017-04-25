ab713dce38ad414f7bf089fe4b6fd65948ad810f
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-04-26 01:00:51 +0200

    add months array

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const model = {
     "form" : document.getElementById('form'),
+    "fullName" : document.getElementById('fullName'),
 
-    "dateMounth" : document.getElementById('dateMounth'),
+    
+    "dateMonth" : document.getElementById('dateMonth'),
 
     "submitBtn" : document.getElementById('btn'),
+
+    "months" : [
+        'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
+    ]
 }
 
 const operator = {
     "checkWindowWidth" : function() {
         var placeholder = window.innerWidth > 900 ? 'January' : '01';
-        model.dateMounth.setAttribute('placeholder', placeholder);
+        model.dateMonth.setAttribute('placeholder', placeholder);
+    },
+    "checkName" : function() {
+        var firstLastName = model.fullName.value.split(" ");
+        if (firstLastName.length < 2) {
+            model.fullName.style.borderBottom = "1px solid red";
+            model.fullName.value = "";
+            model.fullName.setAttribute('placeholder', "Put your first and last name");
+        } else {
+            model.fullName.style.borderBottom = "1px solid #531fc0";
+            this.checkMobile();
+        }
+    },
+    "checkMobile" : function() {
+
+    },
+    "checkDate" : function() {
+
     },
     "eventListeners" : function() {
         window.addEventListener('resize', function() {
@@ -17,7 +40,7 @@ const operator = {
         });
         model.form.addEventListener('submit', function(elem) {
             elem.preventDefault();
-            console.log(this);
+            operator.checkName();
             // model.form.submit();
         });
     }
